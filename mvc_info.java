a62cd6094fc9abb712bcf8d1f5d1126ac54ae34b
Mikhail <futurer@inbox.ru>
2023-11-09 18:02:10 +0300

    Update mvc_info.java

--- mvc_info.java
+++ mvc_info.java
@@ -1,11 +1,27 @@
-@Controller // контроллер, специализированный компонент @Component
+@Controller
+-----------
+// контроллер, специализированный компонент @Component
+@Controller
 public class MyController {
     public MyController() {
     }
-
+    
     @RequestMapping({"/"}) // end point URL
     public String showFirstView() {
         return "first-view"; // связь с файлом view
     }
 }
 
+@RequestMapping
+---------------
+// связь с end point URL
+
+@RequestParam
+-------------
+// параметр переданный в контроллер
+
+public String showEmpDetails1(
+    @RequestParam("employeeName") String empName,
+    Model model
+) {
+}
