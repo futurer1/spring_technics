50387ac468152d47d1754f43ea3bc15f4d7c0f8b
Mikhail <futurer@inbox.ru>
2023-11-10 00:35:38 +0300

    form:select, form:options, form:radiobuttons, form:checkboxes

--- mvc_info.java
+++ mvc_info.java
@@ -34,6 +34,11 @@ public String showEmpDetails1(
 public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
 }
 
+|======|
+| FORM |
+|======|
+
+
 Связь объекта модели class Employee с формой через указание modelAttribute
 --------------------------------------------------------------------------
 // view:
@@ -45,3 +50,31 @@ public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
 Name <form:input path="name"/>
 
     
+// model
+private Map<String, String> departments;
+departments = new HashMap<>();
+departments.put("Information Technology", "IT");
+departments.put("Hrenovye Gabotniki", "HR");
+departments.put("Prodajniki", "sales");
+// view
+<form:select path="department">    
+    <form:options items="${employee.departments}"/>
+</form:select>
+
+// model
+private Map<String, String> carBrands;
+carBrands = new HashMap<>();
+carBrands.put("Geely Coolray", "Geely");
+carBrands.put("Haval H9", "Haval");
+carBrands.put("Dongfeng DF6", "Dongfeng");
+// view
+<form:radiobuttons path="carBrand" items="${employee.carBrands}" />
+
+// model
+private Map<String, String> languagesList;
+languagesList = new HashMap<>();
+languagesList.put("English", "EN");
+languagesList.put("Deutch", "DE");
+languagesList.put("French", "FR");
+// view
+<form:checkboxes path="languages" items="${employee.languagesList}"/>
