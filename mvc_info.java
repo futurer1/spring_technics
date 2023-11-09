0cb26f0e7d456d14e665f45f631244d63d53cbe8
Mikhail <futurer@inbox.ru>
2023-11-09 19:17:00 +0300

    Update mvc_info.java

--- mvc_info.java
+++ mvc_info.java
@@ -28,7 +28,20 @@ public String showEmpDetails1(
 
 @ModelAttribute
 ---------------
-// при работе с формами @ModelAttribute в параметре метода даёт доступ к объекту Модели class Employee
+// Controller при работе с формами @ModelAttribute в параметре метода даёт доступ к объекту Модели class Employee
+// controller:
 @RequestMapping("/showDetails")
 public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
 }
+
+Связь объекта модели class Employee с формой через указание modelAttribute
+--------------------------------------------------------------------------
+// view:
+<form:form action="showDetails" modelAttribute="employee">
+
+Доступ к значениям через геттер объекта Employee.getName()
+----------------------------------------------------------
+// view:
+Name <form:input path="name"/>
+
+    
