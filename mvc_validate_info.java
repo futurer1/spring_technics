0c6d089a70f21c0739f9cff26dd8c4b6e0586101
Mikhail <futurer@inbox.ru>
2023-11-13 22:36:00 +0300

    custom validation annotation

--- mvc_validate_info.java
+++ mvc_validate_info.java
@@ -41,3 +41,48 @@ private int salary;
 @Pattern(regexp = "\\d{3}-\\d{2}-\\d{2}", message = "Phone is incorrect. Pattern: XXX-XX-XX")
 private String phoneNumber;
 
+------------------------------------------
+// Создание своих аннотаций для валидации
+------------------------------------------
+
+@CheckEmail(value = "abc.com", message = "email end abc.com")
+private String email;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = CheckEmailValidator.class) // указан класс CheckEmailValidator, который будет валидировать
+public @interface CheckEmail {
+    public String value() default "xyz.com";
+
+    public String message() default "end email is not xyz.com";
+
+    public Class<?>[] groups() default {};
+
+    public Class<? extends Payload>[] payload() default {};
+}
+
+public class CheckEmailValidator // класс валидатор
+        // CheckEmail - аннотация, которая будет обрабатываться данным классом
+        // String - тип данных, которые будут обрабатываться
+        implements ConstraintValidator<CheckEmail, String> {
+    /**
+     * конечная часть email после символа @
+     */
+    private String endOfEmail;
+
+    @Override
+    public void initialize(CheckEmail checkEmail) {
+        endOfEmail = checkEmail.value(); // значение поля из аннотации
+    }
+
+    @Override
+    public boolean isValid(
+            String enteredValue,
+            ConstraintValidatorContext constraintValidatorContext
+    ) {
+
+        return enteredValue.endsWith(endOfEmail);
+    }
+}
+
+        
